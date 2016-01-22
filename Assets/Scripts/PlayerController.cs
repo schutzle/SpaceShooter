8d0bbd93b7ed653ffbbc113af74d46d5ae4fdbb9
Marcel Schütz <m.schuetz@haptdikdesign.com>
2016-01-22 18:55:32 +0100

    Initial

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary {
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+
+	public float fireRate;
+	private float nextFire = 0f;
+
+	private Rigidbody rb;
+	private AudioSource audio;
+
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+		audio = GetComponent<AudioSource> ();
+	}
+
+
+	void FixedUpdate() {
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (h, 0f, v);
+
+		rb.velocity = movement * speed;
+
+		rb.position = new Vector3
+		(
+			Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax),
+			0f,
+			Mathf.Clamp (rb.position.z, boundary.zMin, boundary.zMax)
+		);
+
+		rb.rotation = Quaternion.Euler (0f, 0f, rb.velocity.x * -tilt);
+	}
+
+	void Update() {
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+			audio.Play ();
+		}
+	}
+}
